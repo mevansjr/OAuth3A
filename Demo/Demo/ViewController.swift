b47efc5b113124b6ddaae51cb6e00471c30fdf46
Mark Evans <Evans@MarksMBPRetina-5.frontierlocal.net>
2018-10-08 19:26:06 -0400

    demo updates

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 import OAuth3A
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
 
         ClientService.shared.loginAnonymous { (success, _) in
             if let token = success as? Token, let json = token.toJSONString(prettyPrint: true) {
